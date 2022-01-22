e3b98470f1e624ec463423c42d7b813e8d45cfdc
vinnyA3 <vincent.aceto@gmail.com>
2022-01-21 23:35:59 -0500

    feat: initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,26 @@
+Anime Series Downloader
+=======================
+
+A simple node script that downloads your desired anime series from `gogoanimes`.
+
+> :warning: This script / tool will be improved in the near future. A modification of
+> source will be necessary to download the series of your choice; out of the box, the
+> script will only download Neon Genesis Evangelion (eps 2 - 26).
+
+## Dependencies
+
+:warning: First, make sure you have the following installed on your machine:
+* [yt-dlp](https://github.com/yt-dlp/yt-dlp) - a youtube download fork with additional fixes and features.
+* nodejs - the latest LTS should be good
+
+## Installation and Usage
+
+**Install**:
+1. Clone repo & `cd anime-series-dl`
+2. Install script dependencies: `node install`
+
+**Usage**:
+1. Optionally modify the `BASE_URL` to your url of choice (line `5` of `index.js`)
+2. Optionally modify the download destination folder (line `74` of `index.js`)
+3. In your terminal: `node index.js`
+4. Pour a :beer:, you'll be waiting a bit.
---
+++ index.js
@@ -0,0 +1,89 @@
+const https = require("https");
+const { spawn } = require("child_process");
+const cheerio = require("cheerio");
+
+const BASE_URL = "https://ww2.gogoanimes.org/watch/neon-genesis-evangelion-dub";
+
+// TODO: once we iterate on & improve the script, we're going to:
+// * read the url as a commnad line argument
+// * generalize the creation of the episode list
+const evangelionDubEpisodeList = ((baseUrl) => {
+  const episodeCount = 27;
+  let episodeList = [];
+
+  for (let i = 1; i < episodeCount; i++) {
+    episodeList.push(`${baseUrl}-episode-${i + 1}`);
+  }
+
+  return episodeList;
+})(BASE_URL);
+
+function queryEpisodePageForVideoSrc(html) {
+  const $ = cheerio.load(html);
+  const videoDiv = $(".play-video");
+  const iFrame = videoDiv.find("iframe");
+
+  if (iFrame && iFrame.length) {
+    return iFrame.attr("src");
+  }
+
+  return null;
+}
+
+const downloadAndSaveVideo = (saveLocation, remoteVideoSrc) =>
+  new Promise((resolve, reject) => {
+    if (!remoteVideoSrc) {
+      return reject("no video source");
+    }
+
+    try {
+      process.chdir(saveLocation); // save destination directory
+
+      const ytDl = spawn("yt-dlp", [remoteVideoSrc]);
+
+      ytDl.stdout.on("data", (buf) => console.log(buf.toString("utf8")));
+      ytDl.on("close", (code) => reject(code));
+      ytDl.on("exit", (code) => resolve(code));
+    } catch (e) {
+      throw new Error(e);
+    }
+  });
+
+const httpGet = (url) => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let data = "";
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => resolve(data));
+      })
+      .on("error", reject);
+  });
+};
+
+(async () => {
+  try {
+    for (let i = 0; i < evangelionDubEpisodeList.length; i++) {
+      const episodeUrl = evangelionDubEpisodeList[i];
+      const episodePageHTML = await httpGet(episodeUrl);
+      const videoSourceUrl = queryEpisodePageForVideoSrc(episodePageHTML);
+
+      console.log("Now downloading: episodeUrl\n");
+
+      if (videoSourceUrl) {
+        await downloadAndSaveVideo("/tmp", videoSourceUrl);
+        console.log("Video Downloaded!");
+      }
+    }
+
+    console.log("Seried Downloaded!");
+  } catch (e) {
+    console.error(
+      `\nOo nyo :3 .. There was an error!\n\nPlease contact the developer
+  and send them the following trace (if any):\n`
+    );
+
+    console.error(e);
+    process.exit(1);
+  }
+})();
