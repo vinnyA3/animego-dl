9a86c2cdf8c1dd624bcdf1cd05d9a945fd121bea
vinnyA3 <vincent.aceto@gmail.com>
2022-01-29 00:04:04 -0500

    feat(bin): check yt-dlp executable exists
    Before doing any work, we're checking for the existence of yt-dlp.
    The logic for checking was moved to a separate module -- note most of
    the feature was copied from the external lib (see comments in
    lib/check-executable); the reason for this: I'd prefer not to depend on
    the external lib .. the logic is fairly simple, and we're using the sync
    implementation only.

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const yargs = require("yargs");
 
 const AnimeDL = require("../lib/anime-dl");
+const checkExecutableSync = require("../lib/check-executable");
 
 const cliOptions = yargs
   .usage("Usage: -d <download destination> -n <anime name>")
@@ -19,13 +20,22 @@ const cliOptions = yargs
     demandOption: true,
   }).argv;
 
-// Initialize
-AnimeDL(cliOptions)
-  .then((successMessage) => {
-    console.log(successMessage);
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// check for 3rd-party executables existence
+if (checkExecutableSync("yt-dlp")) {
+  // Initialize
+  AnimeDL(cliOptions)
+    .then((successMessage) => {
+      console.log(successMessage);
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+} else {
+  console.log(
+    "\n'yt-dlp' is not in your executable $PATH!  Please make sure you have it installed."
+  );
+
+  process.exit(1);
+}
