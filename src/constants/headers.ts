01376220c49a051247ff54037ae95954df9370da
vinnyA3 <vincent.aceto@gmail.com>
2022-06-12 01:05:12 -0400

    fix: broken keys and source extraction
    Gogoanime changed quite a few things ... as a result, the tool was
    rendered completely fucking useless.  This commit updates:
      * keys (temporarily utilizes keys from @justfoolingaround 's extracted keys --
       thanks!!)
      * updates gogo sources
      * implements decryption on new, main gogo embedded source
      * creates new src constants
    
    [#27]

---
+++ src/constants/headers.ts
@@ -0,0 +1,2 @@
+export const USER_AGENT =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
--- src/processing.ts
+++ src/processing.ts
@@ -1,14 +1,20 @@
 import { spawn } from "child_process";
-import { URL } from "url";
-import { createCipheriv } from "crypto";
+import { createCipheriv, createDecipheriv } from "crypto";
 import cheerio from "cheerio";
 
 import utils from "./utils";
-import { GOGO_ENCRYPT_AJAX } from "./constants/urls";
+import { KK_SLIDER } from "./constants/urls";
+import { USER_AGENT } from "./constants/headers";
 import locales from "./locales";
 
 const {
-  general: { compose, stringToNum, removeMatchedPattern, safeJSONParse },
+  general: {
+    compose,
+    stringToNum,
+    removeMatchedPattern,
+    safeJSONParse,
+    isStringEmpty,
+  },
   http: { httpGet },
 } = utils;
 
@@ -41,13 +47,20 @@ const stripNewlinesAndSpacesToNum: (s: string) => number = compose(
   removeMatchedPattern(/[\n\s]+/)
 );
 
-const queryEpisodeDetailsPageForVideoSrc = (html: string): string | null => {
-  const $ = cheerio.load(html);
-  const vidCDNListItem = $(".vidcdn");
-  const vidCDNLink = vidCDNListItem.find("a");
-  const vidSource = vidCDNLink.attr("data-video");
+const detailsEmptyOr404 = (pageHTML: string): boolean => {
+  if (isStringEmpty(pageHTML)) {
+    return true;
+  }
 
-  return `http:${vidSource}` || null;
+  const $ = cheerio.load(pageHTML);
+  const entryTitle = $(".entry-title");
+  return entryTitle && entryTitle.text() === "404";
+};
+
+const getEntryServerUrl = (pageHTML: string): URL | null => {
+  const $ = cheerio.load(pageHTML);
+  const serverUrl = $("#load_anime > div > div > iframe").attr("src");
+  return serverUrl ? new URL("http:" + serverUrl) : null;
 };
 
 const extractVideoMetadataFromDetailsPage = (
@@ -118,35 +131,61 @@ const downloadAndSaveVideo = (videoSourceUrl: string, videoName: string) =>
     ytDl.on("exit", resolve);
   });
 
-const decryptVideoSourceUrl = async (encryptedSourceUrl: string) => {
-  const ajaxEndpoint = GOGO_ENCRYPT_AJAX;
-  const secret = Buffer.from(
-    "3235373436353338353932393338333936373634363632383739383333323838",
-    "hex"
-  ); // aes256 require secret & iv in [hex]
-  const iv = Buffer.from("34323036393133333738303038313335", "hex");
-  const time = "69420691337800813569";
-  const matchResult = encryptedSourceUrl.match(/id=(.*)=?&token=/i);
-
-  if (matchResult) {
-    const videoId = matchResult[1];
-    const cipher = createCipheriv("aes-256-cbc", secret, iv);
-    let encryptedId = "";
-
-    cipher.update(videoId, "binary");
-    encryptedId += cipher.final("base64");
-
-    return await httpGet(
-      new URL(`${ajaxEndpoint}?id=${encryptedId}&time=${time}`).toString(),
-      {
-        headers: {
-          "x-requested-with": "XMLHttpRequest",
-        },
-      }
-    );
+// TODO: refactor this monstrosity!!
+const decryptVideoSourceUrl = async (entryUrl: URL | null) => {
+  if (!entryUrl) {
+    return null;
   }
 
-  return null;
+  const serverPageResponse = await httpGet(entryUrl.toString(), {
+    headers: { "User-Agent": USER_AGENT },
+  });
+
+  const embedId = entryUrl.searchParams.get("id");
+  const $ = cheerio.load(serverPageResponse);
+  const script = $("script[data-name='episode']").data().value;
+  const secrets = await httpGet(KK_SLIDER);
+  const parsedSecrets = JSON.parse(secrets);
+  const { iv, key, second_key: secondKey } = parsedSecrets;
+  const alg = "aes-256-cbc";
+
+  const cipher = createCipheriv(alg, key, iv); // encrypt ajax param id
+  const decipher = createDecipheriv(alg, key, iv); // decrypt token
+  const decipher2 = createDecipheriv(alg, secondKey, iv); // decrypt ajax return encrypted video source url
+
+  let token = "";
+  // @ts-ignore
+  token += decipher.update(script, "base64url", "utf-8");
+  token += decipher.final();
+
+  let encryptedKey = "";
+  // @ts-ignore
+  encryptedKey += cipher.update(embedId, "binary");
+  encryptedKey += cipher.final("base64");
+
+  const params = `id=${encryptedKey}&alias=${embedId}&token=${token}`;
+  const encAjaxEndpoint = `${entryUrl.protocol}//${entryUrl.hostname}/encrypt-ajax.php?${params}`;
+
+  const payload = await httpGet(encAjaxEndpoint, {
+    headers: {
+      "User-Agent": USER_AGENT,
+      "X-Requested-With": "XMLHttpRequest",
+    },
+  });
+
+  const encryptedSourceString = JSON.parse(payload);
+
+  let decryptedSources = "";
+
+  decryptedSources += decipher2.update(
+    encryptedSourceString.data,
+    "base64url",
+    "utf-8"
+  );
+
+  decryptedSources += decipher2.final();
+
+  return decryptedSources;
 };
 
 const getTargetVideoQualityFromSources = (
@@ -157,9 +196,9 @@ const getTargetVideoQualityFromSources = (
   return defaultVideoRendition?.file;
 };
 
-const getSourcesAndDecrypt = compose(
+const decryptAndGetVideoSources = compose(
   decryptVideoSourceUrl,
-  queryEpisodeDetailsPageForVideoSrc
+  getEntryServerUrl
 );
 
 const parseSourcesAndGetVideo = compose(
@@ -168,9 +207,11 @@ const parseSourcesAndGetVideo = compose(
 );
 
 export default {
+  decryptAndGetVideoSources,
+  detailsEmptyOr404,
   extractVideoMetadataFromDetailsPage,
   getEpisodeRangesFromDetailsPage,
+  getEntryServerUrl,
   downloadAndSaveVideo,
-  getSourcesAndDecrypt,
   parseSourcesAndGetVideo,
 };
