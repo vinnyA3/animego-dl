cab331eab12cf8ac95a8126a328354a4042386f9
vinnyA3 <vincent.aceto@gmail.com>
2022-07-18 00:07:57 -0400

    refactor: action creators generator

--- src/state/cli/actions.ts
+++ src/state/cli/actions.ts
@@ -1,20 +1,17 @@
+import { makeActionCreator } from "@utils/state";
+
 const SET_SHOULD_DOWNLOAD = "SET_SHOULD_DOWNLOAD";
 const SET_INPUT_ANIME_NAME = "SET_INPUT_ANIME_NAME";
 const SET_SELECTED_TITLE = "SET_SELECTED_TITLE";
 
+const setInputAnimeName = makeActionCreator(SET_INPUT_ANIME_NAME, "payload");
+const setSelectedTitle = makeActionCreator(SET_SELECTED_TITLE, "payload");
+const setShouldDownload = makeActionCreator(SET_SHOULD_DOWNLOAD, "payload");
+
 export const actionCreators = {
-  setInputAnimeName: (animeName: string) => ({
-    type: SET_INPUT_ANIME_NAME,
-    payload: animeName,
-  }),
-  setSelectedTitle: (title: string) => ({
-    type: SET_SELECTED_TITLE,
-    payload: title,
-  }),
-  setShouldDownload: (shouldDownload: boolean) => ({
-    type: SET_SHOULD_DOWNLOAD,
-    payload: shouldDownload,
-  }),
+  setInputAnimeName,
+  setSelectedTitle,
+  setShouldDownload,
 };
 
 export default {
